ef3cf2487f6c71a5d13368bb5d48e3d5406c7d57
PradeepM <pradeepmanimaran@gmail.com>
2025-07-07 23:24:15 +0800

    fixed some small signupflow bug

---
+++ index.js
@@ -0,0 +1,6 @@
+import { registerRootComponent } from 'expo';
+import App from './App';
+
+// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
+// It also makes sure your app works the same in Expo Go, development builds, and production APKs.
+registerRootComponent(App); 
