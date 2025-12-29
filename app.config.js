7ff3dad24348d44df62a87717835e3d69351cb7f
Your Name <your.email@example.com>
2025-12-30 00:50:52 +0800

    build number to 20

--- app.config.js
+++ app.config.js
@@ -19,7 +19,7 @@ export default {
     ios: {
       supportsTablet: true,
       bundleIdentifier: "com.vybr.music",
-      buildNumber: "20",
+      buildNumber: "21",
       associatedDomains: ["applinks:vybr.app"],
       infoPlist: {
         ITSAppUsesNonExemptEncryption: false,
